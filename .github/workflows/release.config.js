2ed514918828bbaa371a5815bb54dedfb80a70b1
michnae <mich.nae@t-online.de>
2022-03-30 16:39:45 +0200

    fix: fix

--- .github/workflows/release.config.js
+++ .github/workflows/release.config.js
@@ -1,18 +1,18 @@
 const { buildQueries } = require("@testing-library/react");
 
 module.exports = {
-  "branches": "master",
-  "repositoryUrl": "https://github.com/michnae/react-app/",
-  "plugins": [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
+  branches: "master",
+  repositoryUrl: "https://github.com/michnae/react-app/",
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
     [
-      "@semantic-release/github", {
-        "assets": [
+      '@semantic-release/github', {
+        'assets': [
             { "path": "build.zip", "label": "Build" },
             { "path": "coverage.zip", "label": "Coverage" }
         ]
       }
-    ]
+    ],
   ]
-};
+}
