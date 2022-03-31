16ba49418d3ac06877bce6439f206d8679b6b4ef
michnae <mich.nae@t-online.de>
2022-03-31 20:53:13 +0200

    fix: new Release

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react michael/i);
+  const linkElement = screen.getByText(/learn react michnae/i);
   expect(linkElement).toBeInTheDocument();
 });
